7ba1d37e0309eb03edce0bab01b6979ea98171db
Haoran <haoran@untoyou.net>
2022-02-15 10:51:22 +1100

    Add namespace for Model base class
    Hey Herald!
    Some of these models are missing the namespace, which is important to find `lib/Chargebee/Model.php` (https://github.com/heraldsuva/chargebee-php/blob/master/lib/ChargeBee/Model.php).
    Please also add these to the other models that are missing the namespace declarations. Thanks.

--- lib/ChargeBee/Models/ItemPriceAccountingDetail.php
+++ lib/ChargeBee/Models/ItemPriceAccountingDetail.php
@@ -3,6 +3,7 @@
 namespace ChargeBee\ChargeBee\Models;
 use ChargeBee\ChargeBee\Request;
 use ChargeBee\ChargeBee\Util;
+use ChargeBee\ChargeBee\Model;
 
 class ItemPriceAccountingDetail extends Model
 {
